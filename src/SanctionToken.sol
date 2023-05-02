f9e2794beb10467a59893cc9da2e18174a650f22
Brian Mullin <brmullin@email.msmary.edu>
2023-05-01 22:07:13 -0400

    added bannedAddresses list and contructor

--- src/SanctionToken.sol
+++ src/SanctionToken.sol
@@ -9,6 +9,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SanctionsToken is ERC20, Ownable {
     
+    mapping(address => bool) private bannedAddresses;
+
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+
     function transfer() public override returns (bool) {
 
     }
