74e53877de9c62fadfd86d4419595431d524f2a2
Brian Mullin <brmullin@email.msmary.edu>
2023-05-01 22:34:58 -0400

    add method stubs to GodMode contract

--- src/GodMode.sol
+++ src/GodMode.sol
@@ -4,6 +4,18 @@ pragma solidity ^0.8.13;
 ///@title Token with god mode. A special address is able to transfer tokens between addresses at will.
 ///@author Brian Mullin
 
-contract GodMode {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GodMode is ERC20, Ownable {
     
+    function transfer() {
+
+    }
+
+    function transferFrom() {
+
+    }
+
+    function setTransferFromAddress() {}
 }
