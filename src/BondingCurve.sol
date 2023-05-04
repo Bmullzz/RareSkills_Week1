95aeb95ac7156d85746dd10edc195020e68e863e
Brian Mullin <brmullin@email.msmary.edu>
2023-05-03 23:57:04 -0400

    stubbed BondingCurve contract

--- src/BondingCurve.sol
+++ src/BondingCurve.sol
@@ -1,9 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 ///@title Token sale and buyback with bonding curve.
 ///@author Brian Mullin
 
-contract BondingCurve {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC1363/IERC1363Receiver.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BondingCurve is IERC1363Receiver, Ownable {
     
+    constructor() {
+
+    }
+
+    function buy() public {
+
+    }
+
+    function sell() public {
+        
+    }
 }
